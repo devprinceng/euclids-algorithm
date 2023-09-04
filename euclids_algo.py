76b5c3d7ca1fd882236006a9633d15c8b7f0e972
devprinceng <devprinceng@gmail.com>
2023-09-04 04:54:24 +0100

    updated code

--- euclids_algo.py
+++ euclids_algo.py
@@ -3,28 +3,28 @@
 def gcd (m , n):
     #check if either m or n is negative
     if m < 0 or n < 0:
-        raise ValueError("\nError: Arguments passed must be positive integers only")
+        raise ValueError("\nError: Input values must be positive integers only")
     else:
         #loop through until n is equals 0
         while n != 0:
-            #store the initial value of n in a temp variable
-            temp = n
             # store the remainder of the division of m by n
             remainder = m % n
-            #update m to hold the previous value of n, temp
-            m = temp
+            #update m to hold the value of n
+            m = n
             #set n to hold value of the remainder of the division of m by n
             n = remainder
-            
-        return m #m contains the gcd, since n is now 0 after loop is completed
+        
+        # m contains the gcd, since n is 0 when the loop is executed    
+        return m 
 
-
-print("*******EUCLID'S ALGORITHM SOLUTION USING WHILE LOOP IN PYTHON*****\n")
-
+#Lets test what we have done
+print("\n*******EUCLID'S ALGORITHM SOLUTION WITH WHILE LOOP IN PYTHON*****\n")
+# get the inputs from the user & calculate GCD
 try:
     m = int(input('Enter the First Positive Integer: '))
     n = int(input('Enter the Second Positive Integer: '))
 
-    print(f'the gcd of {m} and {n} is: {gcd(m,n)}')
+    #display the GCD of the two numbers 
+    print(f'\nThe GCD of {m} and {n} is: {gcd(m,n)}')
 except ValueError as e:
     print(e)
--- euclids_algo_recursion.py
+++ euclids_algo_recursion.py
@@ -8,15 +8,17 @@ def gcd(m,n):
         return m
     else:
         remainder = m % n
-        #change n to m position and our remainder in n position
+        #call the function again then change n to m position and our remainder in n position
         return gcd(n, remainder)
 
-print("*******EUCLID'S ALGORITHM SOLUTION USING RECURSION IN PYTHON*****\n")
-
+#Lets test what we have done
+print("\n*******EUCLID'S ALGORITHM SOLUTION USING RECURSION IN PYTHON*****\n")
+# get the inputs from the user & calculate GCD
 try:
     m = int(input('Enter the First Positive Integer: '))
     n = int(input('Enter the Second Positive Integer: '))
     
-    print(f'the gcd of {m} and {n} is: {gcd(m,n)}')
+    #display the GCD of the two numbers    
+    print(f'\nThe GCD of {m} and {n} is: {gcd(m,n)}')
 except ValueError as e:
     print(e)
